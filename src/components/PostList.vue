e36e6554e89856966927142d61d47fb83e346594
Sanjar Raxmatov <raxmatovsanjarr@gmail.com>
2023-03-20 22:08:32 +0500

    first commit

---
+++ src/components/PostList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div v-if="posts.length > 0">
+    <h3>Список пользователей</h3>
+    <transition-group name="list">
+      <post-item
+        v-for="post in posts"
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+      />
+    </transition-group>
+  </div>
+  <h2 v-else style="color: red">
+    Список пользователей пуст
+  </h2>
+</template>
+
+<script>
+import PostItem from "@/components/PostItem";
+export default {
+  components: {PostItem},
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.4s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.list-move {
+  transition: transform 0.4s ease;
+}
+</style>
---
+++ src/router/router.js
@@ -0,0 +1,33 @@
+import Main from "@/pages/Main";
+import {createRouter, createWebHistory} from "vue-router";
+import PostPage from "@/pages/PostPage";
+import PostIdPage from "@/pages/PostIdPage";
+import PostPageWithStore from "@/pages/PostPageWithStore";
+
+
+const routes = [
+    {
+        path: '/',
+        component: Main
+    },
+    {
+        path: '/posts',
+        component: PostPage
+    },
+    {
+        path: '/posts/:id',
+        component: PostIdPage
+    },
+    {
+        path: '/store',
+        component: PostPageWithStore
+    },
+
+]
+
+const router = createRouter({
+    routes,
+    history: createWebHistory(process.env.BASE_URL)
+})
+
+export default router;
